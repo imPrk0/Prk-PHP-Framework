1d48b2ca887c06c0f45a5f1297028a38b98af083
Prk <71644464+BiliPrk@users.noreply.github.com>
2022-09-01 12:52:25 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Name: Config File by Prk PHP Framework
+ * Author: Prk
+ * Website: https://imprk.me/
+ * Date: 2018-01-42
+ * Location: Guangzhou Guangdong, People's Republic of China
+ * Update: https://github.com/BiliPrk/Prk-PHP-Framework
+ */
+
+// database
+$db_config = [
+    'name'      =>  '',
+    'username'  =>  '',
+    'password'  =>  '',
+    'prefix'    =>  ''
+];
+
+// safe
+$safe_config = [
+    'password_hash' =>  '',
+    'password_salt' =>  ''
+];
